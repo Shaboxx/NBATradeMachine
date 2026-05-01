df56872013e49294af7e874ef74037f5529155af
Shawn Vazin <shawn.vazin97@gmail.com>
2026-05-01 18:37:31 -0500

    Initial Commit

---
+++ NBATradeMachine/NBATradeMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBATradeMachineApp.swift
+//  NBATradeMachine
+//
+//  Created by Shawn Vazin on 5/1/26.
+//
+
+import SwiftUI
+
+@main
+struct NBATradeMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NBATradeMachineTests/NBATradeMachineTests.swift
@@ -0,0 +1,17 @@
+//
+//  NBATradeMachineTests.swift
+//  NBATradeMachineTests
+//
+//  Created by Shawn Vazin on 5/1/26.
+//
+
+import Testing
+@testable import NBATradeMachine
+
+struct NBATradeMachineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
